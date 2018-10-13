d0df3db592ba326e36d16f6f74165439aba08bc7
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2018-10-13 20:16:27 -0300

    LAB05 - Parceiros de Telecomunicaçoes

--- LPA/Python/LAB05/Lab05-Parceiros_de_Telecomunicacoes.py
+++ LPA/Python/LAB05/Lab05-Parceiros_de_Telecomunicacoes.py
@@ -1,21 +1,29 @@
 import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def mostra(grafo):
+    nx.draw(grafo)
+    plt.show()
+# end mostra()
 
 
 ''' 
     Main
     N = numero total de empresas clientes
     P = total de pares
-    K =  
+    K = conexoes
 '''
 if __name__ == '__main__':
     # lendo linha
     linha = input("")
 
+    # le ate' ser diferente de 0 0 0
     while linha != "0 0 0":
         # pegando cada variavel
         n, p, k = linha.split(" ")
 
-        # converte
+        # converte pra inteiro
         n = int(n)
         p = int(p)
         k = int(k)
@@ -38,24 +46,22 @@ if __name__ == '__main__':
             Grafo.add_edge(a, b)
         # end for
 
-        # listando arestas
-        a = Grafo.nodes()
-        print(a)
-        # remocao de nodes
-        for i in range(1, n):
-            # se node possui um numero menor do que K, remove
-            # if(Grafo.adjacency(i)):
-                Grafo.remove_node(i)
-            # end if
-        # end for
+        # pegando node para remover
+        remove = [node for node, degree in Grafo.degree if degree < k]
+
+        # removendo nodes
+        while remove:
+            Grafo.remove_nodes_from(remove)
+            remove = [node for node, degree in Grafo.degree if degree < k]
+        # end while
+
+        # pegando o tamanho do subgrafo
+        tam = len(max(nx.connected_components(Grafo), key=len)) if Grafo else 0
+        print(tam)
 
         # lendo linha
         linha = input("")
     # end while
-
-
-
-    # tam = max(nx.connected_component_subgraphs(Grafo), key=len)
 # end main
 
 
