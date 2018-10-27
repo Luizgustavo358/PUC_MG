181ed993e38650411f94f60a3be54c82157feca5
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2018-10-27 16:37:51 -0300

    TP1 ES3

---
+++ ES3/TP1/TP1-EPF/scripts/processElementData.js
@@ -0,0 +1,64 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_wBKlAdKJEeiN55mqMrMx-g,_9fa54dKJEeiN55mqMrMx-g,_BsglYdKKEeiN55mqMrMx-gActivity"]="./../../PluginTP1/deliveryprocesses/resources/habilidades de depuração e diagnóstico_FB617E33_fb2ac45d_Activity.jpeg"
+contentPage.processPage.imageFiles["_wBKlAdKJEeiN55mqMrMx-g,_9fa54dKJEeiN55mqMrMx-g,_BsglYdKKEeiN55mqMrMx-gActivityDetail"]="./../../PluginTP1/deliveryprocesses/resources/habilidades de depuração e diagnóstico_FB617E33_fb2ac45d_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_wBKlAdKJEeiN55mqMrMx-g,_9fa54dKJEeiN55mqMrMx-g,_FchTUdKKEeiN55mqMrMx-gActivity"]="./../../PluginTP1/deliveryprocesses/resources/habilidades de depuração e diagnóstico_D94A346A_d9137a94_Activity.jpeg"
+contentPage.processPage.imageFiles["_wBKlAdKJEeiN55mqMrMx-g,_9fa54dKJEeiN55mqMrMx-g,_FchTUdKKEeiN55mqMrMx-gActivityDetail"]="./../../PluginTP1/deliveryprocesses/resources/habilidades de depuração e diagnóstico_D94A346A_d9137a94_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_wBKlAdKJEeiN55mqMrMx-g,_9fa54dKJEeiN55mqMrMx-gActivity"]="./../../PluginTP1/deliveryprocesses/resources/Teste_3C9257A2_d15f59d6_Activity.jpeg"
+contentPage.processPage.imageFiles["_wBKlAdKJEeiN55mqMrMx-g,_9fa54dKJEeiN55mqMrMx-g,_EC8RQdKKEeiN55mqMrMx-gActivity"]="./../../PluginTP1/deliveryprocesses/resources/habilidades de depuração e diagnóstico_A3B14BDD_a37a9207_Activity.jpeg"
+contentPage.processPage.imageFiles["_wBKlAdKJEeiN55mqMrMx-g,_9fa54dKJEeiN55mqMrMx-g,_EC8RQdKKEeiN55mqMrMx-gActivityDetail"]="./../../PluginTP1/deliveryprocesses/resources/habilidades de depuração e diagnóstico_A3B14BDD_a37a9207_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_wBKlAdKJEeiN55mqMrMx-gActivity"]="./../../PluginTP1/deliveryprocesses/resources/ProcTeste_4E8E13E0_4e8e13e0_Activity.jpeg"
+contentPage.processPage.elementUrls["_BsglYdKKEeiN55mqMrMx-g"] = ["Definir Missão da Avaliação","PluginTP1/deliveryprocesses/habilidades de depuração e diagnóstico_FB617E33.html"];
+contentPage.processPage.elementUrls["_X_-ndNKKEeiN55mqMrMx-g"] = ["Plano de Iteração","PluginTP1/deliveryprocesses/plano_de_iteracao_33422F04.html"];
+contentPage.processPage.elementUrls["_mQDa0dKKEeiN55mqMrMx-g"] = ["Plano de testes","PluginTP1/deliveryprocesses/plano_de_testes_EAB78287.html"];
+contentPage.processPage.elementUrls["_lUP54NKKEeiN55mqMrMx-g"] = ["Build","PluginTP1/deliveryprocesses/build_3C91519A.html"];
+contentPage.processPage.elementUrls["_rTk_ENKKEeiN55mqMrMx-g"] = ["Analisar teste de falha","PluginTP1/deliveryprocesses/analisar_teste_de_falha_90D06591.html"];
+contentPage.processPage.elementUrls["_wBKlAdKJEeiN55mqMrMx-g"] = ["ProcTeste","PluginTP1/deliveryprocesses/ProcTeste_4E8E13E0.html"];
+contentPage.processPage.elementUrls["_X_9ZUNKKEeiN55mqMrMx-g"] = ["Identificar Motivadores do Teste","PluginTP1/deliveryprocesses/identificar_motivadores_do_teste_78FC5893.html"];
+contentPage.processPage.elementUrls["_qUaoIdKKEeiN55mqMrMx-g"] = ["Tester","PluginTP1/deliveryprocesses/tester_E617F95F.html"];
+contentPage.processPage.elementUrls["_qUbPNNKKEeiN55mqMrMx-g"] = ["Suite de teste","PluginTP1/deliveryprocesses/suite_de_teste_CE584FE4.html"];
+contentPage.processPage.elementUrls["_lUP55NKKEeiN55mqMrMx-g"] = ["Log de Teste","PluginTP1/deliveryprocesses/log_de_teste_12333B9.html"];
+contentPage.processPage.elementUrls["_qUbPMtKKEeiN55mqMrMx-g"] = ["Plano de testes","PluginTP1/deliveryprocesses/plano_de_testes_2FD61EB.html"];
+contentPage.processPage.elementUrls["_EC8RQdKKEeiN55mqMrMx-g"] = ["Validar Estabilidade do Build","PluginTP1/deliveryprocesses/habilidades de depuração e diagnóstico_A3B14BDD.html"];
+contentPage.processPage.elementUrls["_lUPS0NKKEeiN55mqMrMx-g"] = ["Implementar testes","PluginTP1/deliveryprocesses/implementar_testes_44413DBC.html"];
+contentPage.processPage.elementUrls["_sTpU8NKKEeiN55mqMrMx-g"] = ["Verificar alterações na implementação ","PluginTP1/deliveryprocesses/verificar_alteracoes_na_implementacao_996F02EE.html"];
+contentPage.processPage.elementUrls["_sTrKINKKEeiN55mqMrMx-g"] = ["Resultados de teste","PluginTP1/deliveryprocesses/resultados_de_teste_6DBAE231.html"];
+contentPage.processPage.elementUrls["_sTrKItKKEeiN55mqMrMx-g"] = ["Resumo de avaliação de teste","PluginTP1/deliveryprocesses/resumo_de_avalicao_de_teste_66F1D657.html"];
+contentPage.processPage.elementUrls["_tKFNsNKKEeiN55mqMrMx-g"] = ["Gerente de Teste","PluginTP1/deliveryprocesses/gerente_de_teste_5BFD614C.html"];
+contentPage.processPage.elementUrls["_qUbPMNKKEeiN55mqMrMx-g"] = ["Build","PluginTP1/deliveryprocesses/build_9C66DC5.html"];
+contentPage.processPage.elementUrls["_mQDa0NKKEeiN55mqMrMx-g"] = ["Executar o conjunto de testes","PluginTP1/deliveryprocesses/executar_o_conjunto_de_testes_FC1E6E71.html"];
+contentPage.processPage.elementUrls["_sTp8ANKKEeiN55mqMrMx-g"] = ["Analista de Teste","PluginTP1/deliveryprocesses/analista_de_teste_3642E328.html"];
+contentPage.processPage.elementUrls["_X_-nctKKEeiN55mqMrMx-g"] = ["Plano de desenvolvimento de software","PluginTP1/deliveryprocesses/plano_de_desenvolvimento_de_software_67E7410B.html"];
+contentPage.processPage.elementUrls["_qUbPONKKEeiN55mqMrMx-g"] = ["Modelo de análise de workload","PluginTP1/deliveryprocesses/modelo_de_anlise_de_workload_92EA3203.html"];
+contentPage.processPage.elementUrls["_pNYuoNKKEeiN55mqMrMx-g"] = ["Analista de Teste","PluginTP1/deliveryprocesses/analista_de_teste_DB619C4B.html"];
+contentPage.processPage.elementUrls["_mQDa09KKEeiN55mqMrMx-g"] = ["Suite de teste","PluginTP1/deliveryprocesses/suite_de_teste_5E2F385C.html"];
+contentPage.processPage.elementUrls["_ZaLUkNKKEeiN55mqMrMx-g"] = ["Identificar Alvos do Teste","PluginTP1/deliveryprocesses/identificar_alvos_do_teste_B07227C5.html"];
+contentPage.processPage.elementUrls["_qUaoINKKEeiN55mqMrMx-g"] = ["Implementar conjunto de testes","PluginTP1/deliveryprocesses/implementar_conjunto_de_testes_F77EE549.html"];
+contentPage.processPage.elementUrls["_qUbPNtKKEeiN55mqMrMx-g"] = ["Lista de ideias de teste","PluginTP1/deliveryprocesses/lista_de_ideias_de_teste_C78F440A.html"];
+contentPage.processPage.elementUrls["_lUP54tKKEeiN55mqMrMx-g"] = ["Listas de problemas","PluginTP1/deliveryprocesses/listas_de_problemas_35C845C0.html"];
+contentPage.processPage.elementUrls["_YqUk8NKKEeiN55mqMrMx-g"] = ["Acordo de Missão","PluginTP1/deliveryprocesses/acordo_de_missão_9BC0A8F6.html"];
+contentPage.processPage.elementUrls["_X__OgdKKEeiN55mqMrMx-g"] = ["Plano de testes","PluginTP1/deliveryprocesses/plano_de_testes_C58AF1A6.html"];
+contentPage.processPage.elementUrls["_lUPS0dKKEeiN55mqMrMx-g"] = ["Tester","PluginTP1/deliveryprocesses/tester_32DA51D2.html"];
+contentPage.processPage.elementUrls["_pNYHkNKKEeiN55mqMrMx-g"] = ["Determinar resultados do teste","PluginTP1/deliveryprocesses/determinar_resultados_do_teste_A226E4E2.html"];
+contentPage.processPage.elementUrls["_FchTUdKKEeiN55mqMrMx-g"] = ["Testar e Avaliar","PluginTP1/deliveryprocesses/habilidades de depuração e diagnóstico_D94A346A.html"];
+contentPage.processPage.elementUrls["_X_-ncNKKEeiN55mqMrMx-g"] = ["Gerente de Teste","PluginTP1/deliveryprocesses/gerente_de_teste_6EB04CE5.html"];
+contentPage.processPage.elementUrls["_9fa54dKJEeiN55mqMrMx-g"] = ["Teste","PluginTP1/deliveryprocesses/Teste_3C9257A2.html"];
+contentPage.processPage.elementUrls["_nXBC4NKKEeiN55mqMrMx-g"] = ["Analisar falha de teste","PluginTP1/deliveryprocesses/analisar_falha_de_teste_3DF46B7.html"];
+contentPage.processPage.elementUrls["_nXBC4dKKEeiN55mqMrMx-g"] = ["Resultados de teste","PluginTP1/deliveryprocesses/resultados_de_teste_F2785ACD.html"];
+contentPage.processPage.elementUrls["_qUbPOtKKEeiN55mqMrMx-g"] = ["Log de Teste","PluginTP1/deliveryprocesses/log_de_teste_8C212629.html"];
+contentPage.processPage.elementUrls["_tKEmoNKKEeiN55mqMrMx-g"] = ["Determinar resultados do teste","PluginTP1/deliveryprocesses/determinar_resultados_do_teste_1_49B5D8D0.html"];
+contentPage.processPage.elementUrls["_ZaL7oNKKEeiN55mqMrMx-g"] = ["Analista de Teste","PluginTP1/deliveryprocesses/analista_de_teste_A8C23BA3.html"];
+contentPage.processPage.elementUrls["_cIFFQNKKEeiN55mqMrMx-g"] = ["Definir Avaliação e Rastreabilidade necessários","PluginTP1/deliveryprocesses/definir_avaliao_e_rastreabilidade_necessarios_CB36FB95.html"];
+contentPage.processPage.elementUrls["_YqVMANKKEeiN55mqMrMx-g"] = ["Build","PluginTP1/deliveryprocesses/build_528BFDCE.html"];
+contentPage.processPage.elementUrls["_lUP55tKKEeiN55mqMrMx-g"] = ["Resumo de avaliação de teste","PluginTP1/deliveryprocesses/resumo_de_avalicao_de_teste_FA5A27DF.html"];
