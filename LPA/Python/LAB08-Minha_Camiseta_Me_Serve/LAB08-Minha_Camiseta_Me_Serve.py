8e0ff91c47b483d4678bda3ede6c8901600f5004
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2018-10-29 18:04:57 -0300

    LAB08

--- LPA/Python/LAB08-Minha_Camiseta_Me_Serve/LAB08-Minha_Camiseta_Me_Serve.py
+++ LPA/Python/LAB08-Minha_Camiseta_Me_Serve/LAB08-Minha_Camiseta_Me_Serve.py
@@ -2,6 +2,24 @@
 
 
 if __name__ == '__main__':
-    # a
-    a
+    # lendo quantidade
+    quantidade = int(input(""))
+
+    for i in range(0, quantidade):
+        # lendo n, m
+        linha = input("")
+        n, m = linha.split(" ")
+
+        # convertendo para inteiro
+        n = int(m)
+        m = int(m)
+
+        for j in range(0, m):
+            # lendo tamanhos
+            tam = input("")
+
+            # pegando dois tamanhos para o funcionario
+            tam1, tam2 = tam.split(" ")
+        # end for
+    # end for
 # end main
